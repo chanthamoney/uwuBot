ab1e3d4ae6e4b2d209a8c36ba96e4105eaeb00b4
Cassnadra Chanthamontry <CassandraC0717@gmail.com>
2021-09-11 22:04:39 -0500

    add uwu package to uwu discord

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 require("dotenv").config(); //initialize dotenv
 const Discord = require("discord.js"); //import discord.js
+const Uwuifier = require("uwuifier"); // import uwu
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-
+const uwuifier = new Uwuifier({
+  spaces: {
+    faces: 0.5,
+    actions: 0.004,
+    stutters: 0.1,
+  },
+  words: 1,
+  exclamations: 1,
+});
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
+  if (msg.author.username !== "uwu owo") {
+    msg.reply(uwuifier.uwuifySentence(msg.content));
   }
 });
 
